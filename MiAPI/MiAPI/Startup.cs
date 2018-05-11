9c2574c3010a936c55f9c187a91c8bf8fec909e8
Mario <gonzalezbeldadmario@gmail.com>
2018-05-11 11:21:16 +0200

    Añadido middleware

--- MiAPI/MiAPI/Startup.cs
+++ MiAPI/MiAPI/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using MiAPI.Persistence;
@@ -22,6 +23,7 @@ namespace MiAPI
         }
 
         public IConfiguration Configuration { get; }
+        public object StopWatch { get; private set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -31,19 +33,37 @@ namespace MiAPI
                 options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
                 );
 
+            // Inyección de servicios necesarios para la aplicación
             services.AddTransient<ITodoService, TodoService>();
 
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFacetory)
         {
+            loggerFacetory
+                .AddEventSourceLogger()
+                .AddConsole();
+
+            var logger = loggerFacetory.CreateLogger("Profiler");
+
             // Configuración de Pipeline de ASP NET Core
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.Use(async (context, next) =>
+            {
+                var watch = Stopwatch.StartNew();
+                await next();
+                var path = context.Response.StatusCode;
+                var statusCode = context.Response.StatusCode;
+                var logString = $"Pat h= '{path}', status = {statusCode}, time = {watch.Elapsed}";
+
+                logger.LogInformation(logString);
+            });
 
             app.UseMvc();
         }
